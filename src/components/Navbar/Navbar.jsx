a76cc87a0fbb6d6fb24da9bc4f519fac3d32912a
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-06-18 17:16:54 +0530

    [Chore]: Removed hamburgur icon from landing page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useToggle } from "../../hooks";
 import MobileSidebar from "../MobileSidebar/MobileSidebar";
 import "./Navbar.css";
 
 function Navbar() {
   const [isMobileSidebarVisible, toggleMobileSidebar] = useToggle();
+  const { pathname } = useLocation();
   return (
     <>
       <header>
         <div className="header-brand px-3">
-          <div className="menu" onClick={() => toggleMobileSidebar()}>
-            <i className="fas fa-bars"></i>
-          </div>
+          {pathname !== "/" && (
+            <div className="menu" onClick={() => toggleMobileSidebar()}>
+              <i className="fas fa-bars"></i>
+            </div>
+          )}
           <Link to="/" className="btn-link">
             Always Notes
           </Link>
